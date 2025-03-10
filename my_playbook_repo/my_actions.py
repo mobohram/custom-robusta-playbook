f4bf4cf1e33c297b3146455975ab3f089aebd63e
mobohram <mostafa.bohram@certifaction.com>
2025-03-10 15:17:25 +0200

    update action to filter labels

--- my_playbook_repo/my_actions.py
+++ my_playbook_repo/my_actions.py
@@ -27,3 +27,25 @@ def send_filtered_logs(alert: PrometheusKubernetesAlert, params: LogFilterParams
             FileBlock(f"{pod.metadata.name}_filtered_logs.log", filtered_logs),
         ]
     )
+    
+    labels = alert.alert.labels
+    
+    print("Custom action, labels:", labels)
+    
+    desired_labels = {"app", "pod", "type"}
+    
+    filtered_labels = {key: value for key, value in labels.items() if key in desired_labels}
+    
+    alert.add_enrichment(
+        [
+            TableBlock(
+                [[k, v] for (k, v) in filtered_labels.items()],
+                ["label", "value"],
+                table_format=TableBlockFormat.vertical,
+                table_name="*Alert labels*",
+            ),
+        ],
+        annotations={SlackAnnotations.ATTACHMENT: True},
+        enrichment_type=EnrichmentType.alert_labels,
+        title="Alert labels",
+    )
